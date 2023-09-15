0d8371eda52546002cbd028951f6e0fc403ab16f
NITIN YADAV <70438455+Nitinyadav12345@users.noreply.github.com>
2023-09-16 01:28:54 +0530

    initial commit

---
+++ newsfox/src/Components/sppiner.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './sppiner.css'
+const Spinner = ()=> {
+    return (
+        <div className="container d-flex justify-content-center">
+            <div className="loader"></div>
+        </div>
+        
+    )
+  }
+
+  export default Spinner
